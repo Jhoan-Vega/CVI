bce964b9498552135a5f67cc3384711b104c5af2
Jhoan-Vega <57113325+Jhoan-Vega@users.noreply.github.com>
2022-05-12 23:13:46 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,38 @@
+$('#formlogin').submit(function(e){
+    e.preventDefault();
+    var usuario = $.trim($("#usuario").val());
+    var password = $.trim($("#password").val());
+    if(usuario.length == "" || password == ""){
+        Swal.fire({
+            type:'warning',
+        title:'Ingresa un usuario/clave'
+        });
+        return false
+    }else{
+        $.ajax({
+            url:"bd/login.php",
+            type:"POST",
+            datatype:"json",
+            data: {usuario:usuario, password:password},
+            success:function(data){
+                if(data == "null"){
+                    Swal.fire({
+                        type: 'error',
+                        title: 'usuario y/o contraseña incorrectas'
+                    });
+                }else{
+                    Swal.fire({
+                        type: 'success',
+                        title: 'conexion exitosa',
+                        confirmButtonColor: '#3085d6',
+                        confirmButtonText: 'Ingresar'
+                    }).then((result) => {
+                        if(result.value){
+                            window.location.href = 'inicio.php'
+                        }
+                    })
+                }
+            }
+        });
+    }
+});
